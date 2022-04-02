015d5dda06f9decc3d866af920f692b71e50948f
raul reymundi <kinskispiral@gmail.com>
2022-04-02 13:46:28 -0400

    commit tree 9962cae88bea882a09b95d28e169880e373197ee

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = { 
+    name: `Raul`, 
+    streetAddress: `58 Kent St`,
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+        ...employee,
+        [key]: value,
+      };
+    };
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+      employee[key] = value;
+      return employee;
+    };
+
+function deleteFromEmployeeByKey(employee, key, value){
+  const newEmployee = {...employee};
+  delete newEmployee.name;
+  return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+  delete employee.name;
+  return employee;
+};
